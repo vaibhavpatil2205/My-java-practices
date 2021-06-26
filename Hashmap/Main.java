2349ec18b900eca5477ee56c2987f3aef426c6c3
unknown <vaibhavpatil5471@gmail.com>
2021-06-26 19:32:31 +0100

    Added

---
+++ Hashmap/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main {
+  public static void main(String[] args) {
+
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("India", "New Delhi");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+  }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Audi");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Swift");
+    System.out.println(cars);
+  }
+}
